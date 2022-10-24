0b9783d70e5db29fd527cf877f5c019dea619529
niqoaim <116242894+niqoaim@users.noreply.github.com>
2022-10-24 19:10:54 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,36 @@
-import math
 import streamlit as st
-import random
-n = 0
-if st.button('Randomise'):
-    n = random.randrange(1,10)
-else:
-    "click the button to start"
-    
-with st.echo(code_location='below'):
-     guess = st.number_input("Guess 1", min_value=0, max_value=10, value=0, step=1, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
-     while n!= guess:
-          if guess < n:
-             st.write("Too low")
-             break
-             guess = st.number_input("Guess 2", min_value=0, max_value=10, value=0, step=1, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
-          elif guess > n:
-             st.write("Too high!")
-             break
-             guess = st.number_input("Guess 3", min_value=0, max_value=10, value=0, step=1, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
-          else:
-             st.write("Congrats you guessed it!")
 
-st.write(n)
+list1 = [12, 16, 20, 20, 12, 30, 25, 23, 24, 20]
+frequency = {}
+
+st.write(list1)
+if st.button("Mean"):
+    mean = sum(list1)/len(list1)
+    st.write(mean)
+else:
+    "Press to calcualate the Mean"
+
+    
+if st.button("Median"):
+    if len(list1) % 2 == 0:
+        m1 = list1[len(list1)//2]
+        m2 = list1[len(list1)//2 - 1]
+        median = (m1 + m2)/2
+    else:
+        median = list1[len(list1)//2]
+    st.write(median)
+else:
+    "Press to calculate the median"
+
+if st.button("Mode"):
+    for i in list1:
+        frequency.setdefault(i, 0)
+        frequency[i]+=1
+
+    frequent = max(frequency.values())
+    for i, j in frequency.items():
+        if j == frequent:
+            mode = i
+    st.write(mode)
+else:
+    "Press to calculate mode"
