3cd014e22988b8efe4b318795cc9152e70224cfd
niqoaim <116242894+niqoaim@users.noreply.github.com>
2022-10-24 17:31:33 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,17 @@ import pandas as pd
 import streamlit as st
 import random
 
-n = random.randrange(1,10)
 
 with st.echo(code_location='below'):
     guess = st.slider("Guess a Number", 0, 10, 0)
+    n = random.randrange(1,10)
 
     while n!= guess:
         if guess < n:
             st.write("Too low")
-            n == n
             break
         elif guess > n:
             st.write("Too high!")
-            n == n
             break
         else:
             break
