d9eedd89459cd2242a169e4bd5df59f68ed2b3f7
niqoaim <116242894+niqoaim@users.noreply.github.com>
2022-10-24 17:22:44 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,8 @@ with st.echo(code_location='below'):
     while n!= guess:
         if guess < n:
             st.write("Too low")
-            guess = st.slider("Guess a Number", 1, 10, 5)
         elif guess > n:
             st.write("Too high!")
-            guess = st.slider("Guess a Number", 1, 10, 5)
         else:
             break
         
