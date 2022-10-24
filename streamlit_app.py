0c46d34b42dd09ca7dfbd84406ba922d1dc87a7c
niqoaim <116242894+niqoaim@users.noreply.github.com>
2022-10-24 17:02:43 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,20 @@
 import math
 import pandas as pd
 import streamlit as st
+import random
 
+n = random.randrange(1,10)
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    guess = st.slider("Guess a Number", 1, 5000, 2000)
+
+    while n!= guess:
+        if guess < n:
+           print("Too low")
+           guess = int(input("Enter number again: "))
+        elif guess > n:
+            print("Too high!")
+            guess = int(input("Enter number again: "))
+        else:
+        break
+print("you guessed it right!!")
