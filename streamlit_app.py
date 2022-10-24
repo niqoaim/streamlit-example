6c8a6772e42e9dd0f72867a12bb064ce1e84b08e
niqoaim <116242894+niqoaim@users.noreply.github.com>
2022-10-24 18:17:29 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,23 @@
 import math
-import pandas as pd
 import streamlit as st
 import random
 
-
-with st.echo(code_location='below'):
-    
+if st.button('Randomise'):
     n = random.randrange(1,10)
-    guess = st.number_input("Take a guess", min_value=1, max_value=10, value=1, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+else:
+    break
+with st.echo(code_location='below'):
 
-    if guess < n:
-        st.write("Too low")
-        break
-    elif guess > n:
-        st.write("Too high!")
-        break
-    else:
-        break
+     n = random.randrange(1,10)
+     guess = st.number_input("Take a guess", min_value=1, max_value=10, value=1, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+
+     while n!= guess:
+         if guess < n:
+            st.write("Too low")
+            break
+        elif guess > n:
+            st.write("Too high!")
+            break
+        else:
+            break
     st.write(n)
