538f3e73e3b2287110a0766c2d95831ec15d20d3
niqoaim <116242894+niqoaim@users.noreply.github.com>
2022-10-24 18:25:30 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,15 @@ import math
 import streamlit as st
 import random
 
+n = 0
+
 if st.button('Randomise'):
     n = random.randrange(1,10)
 else:
-    "click me"
+    "click the button to start"
     
 with st.echo(code_location='below'):
 
-     n = random.randrange(1,10)
      guess = st.number_input("Take a guess", min_value=0, max_value=10, value=0, step=None, format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
 
      while n!= guess:
