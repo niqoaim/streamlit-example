a2a0a9f5c76e197651f06d2abb9f8c2204c01d9e
niqoaim <116242894+niqoaim@users.noreply.github.com>
2022-10-24 17:50:29 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import random
 with st.echo(code_location='below'):
     
     n = random.randrange(1,10)
-    guess = st.number_input("Take a guess")
+    guess = int(st.number_input("Take a guess"))
 
     while n!= guess:
         if guess < n:
