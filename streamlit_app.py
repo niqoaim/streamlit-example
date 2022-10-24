b1da697517ea6b363eb0aeb57631c68c89f8d5cf
niqoaim <116242894+niqoaim@users.noreply.github.com>
2022-10-24 17:44:47 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,9 @@ import random
 with st.echo(code_location='below'):
     
     n = random.randrange(1,10)
+    guess = st.number_input(label, min_value=1, max_value=10, value=, label_visibility="visible")
 
-    while n!= st.slider("Guess a Number", 0, 10, 0):
+    while n!= guess:
         if guess < n:
             st.write("Too low")
             break
